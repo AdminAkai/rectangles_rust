48f8fb32551227b6e8b8f4f10143950538824084
AdminAkai <akatoirotech@gmail.com>
2024-07-16 05:07:21 -0400

    implementation with area method

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
 fn main() {
@@ -9,9 +16,5 @@ fn main() {
         height: 50,
     };
 
-    println!("The area of the rectangle is {} square pixels.", area(&rect1));
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
 }
